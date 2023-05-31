b8dbf9b6dae6b83b9d5031ea8c289833dc3a0b5f
Yaroslav Shishkin <company-rosstan@yandex.ru>
2023-05-31 13:13:37 +0300

    twenty-nine

---
+++ Task twenty-nine/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.Clear();
+int[] arr = new int[8];
+for(int i = 0; i < arr.Length; i++)
+arr[i] = new Random().Next(1, 9);
+Console.WriteLine($"[{string.Join(", ", arr)}]");
+
