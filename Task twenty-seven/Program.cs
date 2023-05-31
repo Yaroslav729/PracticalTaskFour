332bbaba10ffdcc7470817545e6e5e78cb90c366
Yaroslav Shishkin <company-rosstan@yandex.ru>
2023-05-31 15:49:00 +0300

    twenty-seven

---
+++ Task twenty-seven/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+int len = CountLenght(a);
+int counter = 0;
+int CountLenght(int num)
+{
+int index = 0;
+while(num > 0)
+{
+num /= 10;
+index++;
+}
+return index;
+}
+for(int i = 0; i < len; i++){
+counter += a % 10;
+           a /= 10;
+}
+Console.Write(counter); 
