097c8b14d07a18cc5896f22ecef70a0eec0c0c6b
Yaroslav Shishkin <company-rosstan@yandex.ru>
2023-05-31 10:41:21 +0300

    Twenty-five

---
+++ Task twenty-five/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите степень: ");
+int count = 1;
+int b = int.Parse(Console.ReadLine()!);
+for (int i = 0; i < b; i++){
+count *= a;
+}
+
+
+Console.Write($" {count} ");
